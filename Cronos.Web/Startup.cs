6767f8d4853ff17551ba3a5c3f27657e348ce852
Mike Ruhl <michael.a.ruhl@gmail.com>
2018-02-25 08:07:45 -0500

    Set base solution
    Remove about and contact views
    add fluent spotify middleware

--- Cronos.Web/Startup.cs
+++ Cronos.Web/Startup.cs
@@ -2,8 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FluentSpotifyApi.AuthorizationFlows.AspNetCore.AuthorizationCode.Extensions;
+using FluentSpotifyApi.AuthorizationFlows.AspNetCore.AuthorizationCode.Handler;
+using FluentSpotifyApi.Extensions;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -11,6 +17,8 @@ namespace Cronos.Web
 {
     public class Startup
     {
+        private const string SpotifyAuthenticationScheme = SpotifyDefaults.AuthenticationScheme;
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -21,6 +29,38 @@ namespace Cronos.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services
+                .AddFluentSpotifyClient(clientBuilder => clientBuilder
+                    .ConfigurePipeline(pipeline => pipeline
+                        .AddAspNetCoreAuthorizationCodeFlow(
+                            spotifyAuthenticationScheme: SpotifyAuthenticationScheme)));
+
+            var id = Configuration["SPOTIFY_CLIENTID"];
+            var secret = Configuration["SPOTIFY_CLIENT_SECRET"];
+
+            services
+                .AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(
+                    o =>
+                    {
+                        o.LoginPath = new PathString("/login");
+                        o.LogoutPath = new PathString("/logout");
+                    })
+                .AddSpotify(
+                    SpotifyAuthenticationScheme,
+                    o =>
+                    {
+                        o.ClientId = id;
+                        o.ClientSecret = secret;
+                        o.Scope.Add("playlist-read-private");
+                        o.Scope.Add("playlist-read-collaborative");
+                        o.Scope.Add("playlist-modify-private");
+                        o.Scope.Add("playlist-modify-public");
+                        o.Scope.Add("user-read-private");
+                        o.Scope.Add("user-top-read");
+                        o.SaveTokens = true;
+                    });
+
             services.AddMvc();
         }
 
@@ -36,6 +76,37 @@ namespace Cronos.Web
             {
                 app.UseExceptionHandler("/Home/Error");
             }
+
+            app.UseAuthentication();
+
+            //Spotify Auth Mapping
+            app.Map(
+                "/login",
+                builder =>
+                {
+                    builder.Run(
+                        async context =>
+                        {
+                            // Return a challenge to invoke the Spotify authentication scheme
+                            await context.ChallengeAsync(SpotifyAuthenticationScheme, properties: new AuthenticationProperties() { RedirectUri = "/", IsPersistent = true });
+                        });
+                });
+
+            // Listen for requests on the /logout path, and sign the user out
+            app.Map(
+                "/logout",
+                builder =>
+                {
+                    builder.Run(
+                        async context =>
+                        {
+                            // Sign the user out of the authentication middleware (i.e. it will clear the Auth cookie)
+                            await context.SignOutAsync();
+
+                            // Redirect the user to the home page after signing out
+                            context.Response.Redirect("/");
+                        });
+                });
 
             app.UseStaticFiles();
 
