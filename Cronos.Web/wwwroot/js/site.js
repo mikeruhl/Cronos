2b96cce2252713edf92671c70aa976a4dcb132ce
Mike Ruhl <michael.a.ruhl@gmail.com>
2018-03-18 11:44:44 -0400

    Fix js artist auto-complete add footer
    moved auto-complete to artist.js so it wouldn't load on other pages.  Added footer.  Fixed privacy policy.

--- Cronos.Web/wwwroot/js/site.js
+++ Cronos.Web/wwwroot/js/site.js
@@ -43,43 +43,5 @@ $(document).ready(function () {
     });
 
 
-    $('.artist-suggestion').autocomplete({
-        minLength: 3,
-        source: function(request, response) {
-            var endpoint = $('.artist-suggestion').attr('artist-suggestion-url');
-            console.log("hitting url: " + endpoint);
-            if (endpoint === undefined)
-                return;
-            $.ajax({
-                url: endpoint,
-                type: 'GET',
-                dataType: 'json',
-                data: request,
-                success: function(data) {
-                    response($.map(data,
-                        function(item) {
-                            return {
-                                label: item.name,
-                                id: item.id,
-                                url: item.imgUrl
-                            }
-                        }));
-                }
-            });
-        },
-        select: function(event, ui) {
-            $(".artist-id").val(ui.item.id);
-        }
-    })
-        .autocomplete("instance")._renderItem = function (ul, item) {
-            return $("<li class=\"list-group-item autocomplete-item\">")
-                .append("<div>" + item.label + "</div>")
-                .appendTo(ul);
-        };;
-    //.autocomplete("instance")._renderItem = function(ul, item) {
-    //    return $("<li>")
-    //        .append("<div><img class=\"img img-responsive\" src=\"" + item.url + "\"></img><br>" + item.label + "</div>")
-    //        .appendTo(ul);
-    //};
 
 });
